d67a2d156b06cafe75b472221e4209e7505095a3
lfmcagency <louisfaucher95@gmail.com>
2025-03-18 16:26:20 +0100

    small menu fix

--- src/components/layout/QuickAddMenu.tsx
+++ src/components/layout/QuickAddMenu.tsx
@@ -43,7 +43,7 @@ export default function QuickAddMenu({ isOpen, onClose }: QuickAddMenuProps) {
     },
     {
       name: "Ethos",
-      description: "dsicipline & habits",
+      description: "routines & habits",
       path: "/routine",
       symbol: "Η" // Eta
     },
