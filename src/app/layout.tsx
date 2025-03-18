632e61d82016bd54b1a466c9fe95d4cc450e5036
lfmcagency <louisfaucher95@gmail.com>
2025-03-19 00:21:49 +0100

    build error fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,7 @@ const lucidaTypewriter = localFont({
 })
 
 const lucidaConsole = localFont({
-  src: '../public/fonts/lucida/lucon.ttf',
+  src: '/fonts/lucida/lucon.ttf',
   variable: '--font-body',
   display: 'swap',
 })
